7eb6b8145ef276ae9b818964409a563d394a6b1d
Lars Bonczek <lars.bonczek@gmail.com>
2021-08-03 18:19:32 +0200

    MDL-72271 mod_quiz: fix secure_window close function

--- mod/quiz/module.js
+++ mod/quiz/module.js
@@ -265,7 +265,7 @@ M.mod_quiz.secure_window = {
         }, '#secureclosebutton');
     },
 
-    close: function(Y, url, delay) {
+    close: function(url, delay) {
         setTimeout(function() {
             if (window.opener) {
                 window.opener.document.location.reload();
